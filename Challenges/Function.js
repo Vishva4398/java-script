b3d8576b7746f1ce7f341096a84c15a3a952f185
Vishvajeet <76872492+Vishvajeet4398@users.noreply.github.com>
2023-02-03 00:11:59 +0530

    js

---
+++ Challenges/Function.js
@@ -0,0 +1,31 @@
+//1st challenge
+/*function calculateTotal(subtotal, tax){
+    const total = subtotal + tax;
+    return total;
+}
+console.log(calculateTotal(100, 20));
+*/
+
+
+//2nd challenge
+/*function calculateTotal(subtotal, tax){
+    const total = subtotal + tax;
+    return total;
+}
+const order1 = calculateTotal(200, 10);
+const order2 = calculateTotal(300, 10);
+const order3 = calculateTotal(400, 10);
+console.log(order1, order2, order3);
+*/
+
+
+//refactor challenge
+/*const calculateTotal = function random (subtotal, tax){
+    const total = subtotal + tax;
+    return total;
+}
+const order1 = calculateTotal(200, 10);
+const order2 = calculateTotal(300, 10);
+const order3 = calculateTotal(400, 10);
+console.log(order1, order2, order3);
+*/
---
+++ app.js
@@ -0,0 +1,95 @@
+//Alert box
+
+/*let vardan ="hello india"
+    alert("Please click OK")
+console.log(vardan);*/
+
+
+/*Array in java Script*/
+/*let array = ["golu","sonu","monu","tonny"];
+console.log(array[2]);
+*/
+
+/*push
+const num = [2,4,6,7,8];
+num.push(200);/* add new element in end
+console.log(num);*/
+
+
+
+/*const array1 = [2,4,6,7,8];
+array1.unshift(900);/* add new element in starting*/
+/*console.log(array1);*/
+
+
+/*update */
+/*const arr2 =["math","physics","english"];
+arr2[2] = "chemistry";  
+console.log(arr2);*/
+
+
+
+/////////////////////*Function*/
+/*function FullName(fname,lname) {
+    console.log("Hello everyone my name is:" + fname +" "+ lname);
+};
+FullName("john","petter");*/
+// functions- Declare, invoke
+/*function vardan(){//declare the function
+    console.log("hi folks");
+};
+vardan();//invoke the function*/
+
+
+/* function with Parameters, Arguments*/
+/*function sum(a,b){
+    console.log(a+b);
+};
+sum(10,20);*/
+
+/*function mul(a,b){
+    console.log(a/b);
+};
+mul(20,4);
+*/
+/*function sum(a,b){
+    console.log(a/b);
+};
+div(20,4);
+*/
+
+/*function div(a,b){
+    console.log(a/b);
+};
+div(20,4);
+*/
+
+// function with return value
+
+/*function add(a,b) {
+    let c = a+b;
+    return c;
+}
+let d = add(10,20)
+console.log(d);*/
+
+
+///////Object////////
+/*let person = {firstName:"vardan",
+lastName:"singh",
+age:24};
+console.log(person);*/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
